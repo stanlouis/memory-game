7e0022faf78c545fbf7384c6570c6ed76c521423
Stanley Louis <stanlouis@msn.com>
2018-07-19 21:54:12 -0400

    reduce card sizes

--- src/components/Robot.js
+++ src/components/Robot.js
@@ -3,10 +3,10 @@ import React from "react";
 const Robot = ({ name, email, id, roboClicked }) => {
   return (
     <div
-      className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5"
+      className="tc bg-light-green dib br3 pa2-m ma2 grow bw2 shadow-5"
       onClick={() => roboClicked(id)}
     >
-      <img src={`https://robohash.org/${id}200*200`} alt="robots" />
+      <img src={`https://robohash.org/${id}50*50`} alt="robots" />
       <div>
         <h2>{name}</h2>
       </div>
